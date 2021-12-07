4cee7e45652fce291f6f7a1ef5df09de24b9fe1f
Tatsuya Kosuge <castero1219@gmail.com>
2021-12-07 21:04:29 +0900

    [modify]コードの動作確認以外は実施

--- js/script.js
+++ js/script.js
@@ -1,35 +1,68 @@
-const parent = window.parent.document;
-const x = parent.querySelector("#x").value;
-const y = parent.querySelector("#y").value;
-const buttonW = parent.querySelector("#button-w").value;
-const buttonH = parent.querySelector("#button-h").value;
+let img,bgImage,;
+let imgX = 0;
+let imgY = 0;
 
-let img;
+let array = [
+  [0,0,1,0,0,0,0,0],
+  [1,0,1,0,0,0,0,0],
+  [1,0,1,1,1,0,1,0],
+  [1,0,0,0,0,0,1,0],
+  [1,0,1,1,1,0,1,0],
+  [0,0,0,1,1,0,1,0],
+  [0,1,0,0,0,0,1,0],
+  [0,1,0,1,1,0,1,0]  
+]
+
 
 function setup() {
   const canvas = createCanvas(320, 350);
   const area = document.querySelector("#canvasArea");
   canvas.parent(area);
   const leftButton = setButton("←", {
-    x: x,
-    y: y,
+    x: 0,
+    y: 320,
+  });
+  const topButton = setButton("↑", {
+    x: 80,
+    y: 320,
+  });
+  const rightButton = setButton("→", {
+    x: 160,
+    y: 320,
+  });
+  const bottomButton = setButton("↓", {
+    x: 240,
+    y: 320,
   });
 
   leftButton.parent(area);
+  topButton.parent(area);
+  rightButton.parent(area);
+  bottomButton.parent(area);
+
 }
 
+
 function preload() {
-  img = loadImage("stamp18.png");
+  img = loadImage('stamp18.png');
+  bgImage = loadImage('kosuge-horror.jpg');
 }
 
 function draw() {
   background(220);
+  bgRefresh();
   image(img, 0, 0, 40, 40);
 }
 
-function setButton(label, pos) {
-  const button = createButton(label);
-  button.size(buttonW, buttonH);
-  button.position(pos.x, pos.y);
-  return button;
-}
+// 背景をループで表示させている箇所
+function bgRefresh() {
+  background(240);
+  for(let i=0; i&lt;8; i++){
+    for(let j=0; j&lt;8; j++){
+    if(array[j][i]===1){
+      image(bgImage, i*40, j*40, 40, 40);
+    }
+
+    }
+  }
+}</div>
